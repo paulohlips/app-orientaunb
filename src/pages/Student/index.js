29ebb9462f7802137e9ff13d6a0f1dcffef6b114
Unknown <unknown@example.com>
2020-07-18 23:00:11 -0300

    fix students

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -3,16 +3,24 @@
 /* eslint-disable global-require */
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
-import { BackHandler } from 'react-native';
+import { BackHandler, ScrollView } from 'react-native';
 import useAuth from '../../store';
 import api from '../../services/api';
 
 import {
   Container,
-  CenterView,
-  LogoUnB,
-  LogoLatitude,
-  SimpleText,
+  SubContainer,
+  CardContainer,
+  HeaderTitle,
+  HeaderText,
+  TextView,
+  Text,
+  TextTeste,
+  PickerView,
+  Favorite,
+  CloseView,
+  CloseButton,
+  TextClose,
 } from './styles';
 
 const withZustand = (Comp) => (props) => {
@@ -58,8 +66,55 @@ class Student extends Component {
     return true;
   };
 
+  checkOwner(user, orientationOwner) {
+    if ((user = orientationOwner)) {
+      return true;
+    }
+    return false;
+  };
+
   render() {
-    return <Container />;
+    const { myOrientations, lista } = this.state;
+    const { userData } = this.props;
+    return (
+      <Container>
+          <HeaderTitle>
+            <HeaderText>Minhas Orientações </HeaderText>
+          </HeaderTitle>
+          
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {myOrientations.map((item) =>
+            this.checkOwner(userData.id, item.professor_id) ? (
+                <CardContainer>
+                  <TextView>
+                    <Text b>Departamento: </Text>
+                    <Text>{item.departament}</Text>
+                  </TextView>
+                  <TextView>
+                    <Text b>Título: </Text>
+                    <Text>{item.title}</Text>
+                  </TextView>
+                  <TextView>
+                    <Text b>Resumo: </Text>
+                    <TextTeste>{item.details}</TextTeste>
+                  </TextView>
+                  <CloseView>
+                    <CloseButton
+                      onPress={() => {
+                        this.handleClose(item.id);
+                      }}>
+                      <TextClose>Encerrar orientação</TextClose>
+                    </CloseButton>
+                  </CloseView>
+                </CardContainer>
+              ) : null
+          )}                
+          {!myOrientations ? (
+            <Text>Departamento: Você ainda não possui orientações!</Text>
+          ) : null}
+        </ScrollView>
+      </Container>
+    );
   }
 }
 
--- src/pages/Student/styles.js
+++ src/pages/Student/styles.js
@@ -3,31 +3,87 @@ import normalize from 'react-native-normalize';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 30px;
-  justify-content: center;
-  background: #fff;
-`;
-
-export const CenterView = styled.View`
+  width: 100%;
   align-items: center;
+  justify-content: center;
 `;
 
-export const LogoUnB = styled.Image`
-  width: ${normalize(180)};
-  height: ${normalize(180, 'height')};
+export const SubContainer = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+export const CardContainer = styled.TouchableOpacity`
+  padding: 10px 0px 10px 10px;
+  margin: 20px 10px 5px 10px;
   background: #fff;
-  align-self: center;
-  margin-bottom: 15px;
+  flex-direction: column;
+  border-top-width: 20px;
+  border-width: 2px;
+  elevation: 10;
+  border-color: #0039a6;
 `;
 
-export const SimpleText = styled.Text`
+export const HeaderTitle = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  margin-top: 15px;
+`;
+
+export const HeaderText = styled.Text`
+  text-align: center;
+  color: #0039a6;
   font-weight: bold;
+  font-size: 18px;
 `;
 
-export const LogoLatitude = styled.Image`
-  width: ${normalize(300)};
-  height: ${normalize(160)};
+export const TextView = styled.View`
+  flex-direction: row;
+  flex: 1;
+  margin: 2px 0px 2px 0px;
+`;
+
+export const CloseView = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-right: 10px;
+`;
+
+export const Text = styled.Text`
+  font-weight: ${(props) => (props.b ? 'bold' : 'normal')};
+`;
+
+export const TextTeste = styled.Text`
+  width: 82%;
+`;
+
+export const Favorite = styled.TouchableOpacity`
+  justify-content: flex-end;
+  width: 25px;
+  margin-left: 180px;
+`;
+
+export const CloseButton = styled.TouchableOpacity`
+  width: 140px;
+  margin-top: 10px;
+`;
+
+export const TextClose = styled.Text`
+  font-weight: bold;
+  color: rgba(255, 0, 0, 0.75);
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#000',
+})`
+  height: 45px;
+  width: 80%;
   background: #fff;
-  align-self: center;
-  margin-bottom: 15px;
+  border-radius: 11px;
+  border: 2px solid #000;
+  margin: 25px 0 8px 0;
+  elevation: 6;
 `;
